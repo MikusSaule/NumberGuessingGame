c84108c9f407e4f5abf7ac0ffa5bb4431553feb2
MikusSaule <109538364+MikusSaule@users.noreply.github.com>
2022-11-16 00:00:25 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Random number = new Random();
+        int thenumber = number.nextInt(10);
+        int guess;
+        System.out.println("Guess a the right number!");
+        System.out.println("1-10!");
+        guess = scanner.nextInt();
+        if (guess == thenumber) {
+            System.out.println("You win!");
+        }
+        else if (guess < thenumber){
+            System.out.println("Too small!Try again!");
+        }
+        else if (guess > thenumber){
+            System.out.println("Too big!Try again!");
+        }
     }
 }
